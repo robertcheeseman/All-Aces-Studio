366a6b10e858ea6cc64328fd1c88ce60ef9ccf7b
Robert C <67304122+robertcheeseman@users.noreply.github.com>
2023-01-09 21:52:38 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This is the start of All Aces Studio's new site
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
