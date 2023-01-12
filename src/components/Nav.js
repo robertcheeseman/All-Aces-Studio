4f2d48f09470843390749c1fff9e531c6767bb38
Robert C <67304122+robertcheeseman@users.noreply.github.com>
2023-01-11 22:21:15 -0700

    updates

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,6 +3,19 @@ import { Link } from 'react-router-dom'
 
 export default function Nav() {
   return (
-    <div>Nav</div>
+    <div>
+        <nav className='navbar'>
+            <div className='navcontainer'>
+                <ul>
+                    <li><Link to="/Home">Home</Link></li>
+                    <li><Link to="/Studio">Studio</Link></li>
+                    <li><Link to="/About">About</Link></li>
+                    <li><Link to="/Services">Services</Link></li>
+                    <li><Link to="/Discography">Discography</Link></li>
+                    <li><Link to="/Contact">Contact</Link></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
   )
 }
