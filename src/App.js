96badf61a8072475c691a80455ceb3d4778fd19c
Robert C <67304122+robertcheeseman@users.noreply.github.com>
2023-01-11 22:08:20 -0700

    updating

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav'
+import Home from './views/Home';
+import About from './views/About';
+import Contact from './views/Contact';
+import Discography from './views/Discography';
+import Services from './views/Services';
+import Studio from './views/Studio';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          This is the start of All Aces Studio's new site
-        </p>
-      </header>
+      <BrowserRouter>
+      <Nav />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/About" element={<About />}></Route>
+          <Route path="/Contact" element={<Contact />}></Route>
+          <Route path="/Discography" element={<Discography />}></Route>
+          <Route path="/Services" element={<Services />}></Route>
+          <Route path="/Studio" element={<Studio />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
