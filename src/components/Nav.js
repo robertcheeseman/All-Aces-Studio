958938e995e87b758cdbec66bba2bd29b9cc3a9c
Robert C <67304122+robertcheeseman@users.noreply.github.com>
2023-01-18 22:35:40 -0700

    updating

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ export default function Nav() {
   return (
     <div className='navbackground'>
         <nav className='navbar'>
-          <img src="./assets/marshall_head.jpg" alt="" />
+          <img src="../assets/marshall_head.jpg" alt="" />
             <div className='navcontainer'>
               <p>All Aces Studio</p>
                 <ul>
--- src/views/Discography.js
+++ src/views/Discography.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom'
 
 export default function discography() {
   return (
-    <div>this is the discography</div>
+    <div>this is the discography
+
+      Here we will generate a grid of album artwork showing off records that have been recorded at the studio
+    </div>
   )
 }
