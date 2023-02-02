3f39231aa3279d6e75313e0a7ab24adb10eb8d90
Robert C <67304122+robertcheeseman@users.noreply.github.com>
2023-02-01 22:37:48 -0700

    updating

--- src/views/Services.js
+++ src/views/Services.js
@@ -5,6 +5,7 @@ export default function Services() {
   return (
     <div>
       <img src="/images/All-Aces-Body-Bot.png" alt="" id='ampcab'/>
+      <p>This is where we will list all of the services offered by the studio</p>
     </div>
   )
 }
