571c6e998c684ceb649ebdd9055ed143905df526
Robert C <67304122+robertcheeseman@users.noreply.github.com>
2023-02-02 23:03:24 -0700

    updating

--- src/views/Discography.js
+++ src/views/Discography.js
@@ -6,6 +6,8 @@ export default function discography() {
     <div>this is the discography
 
       Here we will sdf generate a grid of album artwork showing off records that have been recorded at the studio
+      maybe have a cascade of album artwork 
+      perhaps incorporate playing a sample from spotify?
     </div>
   )
 }
