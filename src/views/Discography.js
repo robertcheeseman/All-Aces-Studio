e8acbc8789dd4beb065b11141b93bdb1f75117bb
Robert C <67304122+robertcheeseman@users.noreply.github.com>
2023-02-07 21:52:31 -0700

    updating

--- src/views/Discography.js
+++ src/views/Discography.js
@@ -5,7 +5,7 @@ export default function discography() {
   return (
     <div>this is the discography
 
-      Here we will sdf generate a grid of album artwork showing off records that have been recorded at the studio
+      Here we will generate a grid of album artwork showing off records that have been recorded at the studio
       maybe have a cascade of album artwork 
       perhaps incorporate playing a sample from spotify?
     </div>
