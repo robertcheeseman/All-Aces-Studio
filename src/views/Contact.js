d06f50253b4181c3a97f2b99e30ead673c7515c1
Robert C <67304122+robertcheeseman@users.noreply.github.com>
2023-02-20 22:57:29 -0700

    updating

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -5,7 +5,7 @@ export default function contact() {
   return (
     <div>This is the contact page
 
-      Obviously a contact form of some sort if that's your thing so people can get in touch
+      Obviously a contact form of some sort if that's your thing so people can get in touch with the studio
     </div>
   )
 }
