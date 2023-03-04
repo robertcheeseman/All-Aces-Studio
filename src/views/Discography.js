e251362d80eeb5047758c708ed85a74e961a7085
Robert C <67304122+robertcheeseman@users.noreply.github.com>
2023-03-03 22:57:36 -0700

    updating

--- src/views/Discography.js
+++ src/views/Discography.js
@@ -3,11 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function discography() {
   return (
-    <div>this is the discography
-
-      Here we will generate all grid of album artwork showing off records that have been recorded at the studio
-      maybe have a cascade of album artwork 
-      perhaps incorporate playing a sample from spotify? asadsfdf 
+    <div>this is the discography page
     </div>
   )
 }
