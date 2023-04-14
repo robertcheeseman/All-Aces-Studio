43b2efa9a8e04c953f5c267cb0513092fe09f21f
Robert C <67304122+robertcheeseman@users.noreply.github.com>
2023-04-13 21:31:02 -0600

    updating

--- src/views/Discography.js
+++ src/views/Discography.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function discography() {
   return (
-    <div>this is the discography page for album artwork to show
+    <div>this is the discography page for album artwork to show up
     </div>
   )
 }
